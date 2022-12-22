7bc6aaf9d4f0d481f9b745e51b8221bef4750a1a
Stephen Kinnaird <slkinnaird@gmail.com>
2022-12-22 17:44:31 +0000

    cv upload

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,6 +8,9 @@ const NavBar = () => {
       <a href='https://github.com/skinnaird88' target="_blank">
         <img src='../../github.png' id='github'></img>
       </a>
+      <a href='../../SKinnaird-CV.pdf' target="_blank">
+        <img src='../../cv.png' id='cv'></img>
+      </a>
       <a href='https://www.linkedin.com/in/stephen-kinnaird-a9b713112/' target="_blank">
         <img src='../../linkedin.png' id='linkedin'></img>
       </a>
