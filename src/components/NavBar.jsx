1e1913e063b6ce7046f344e7e2d3021ca4ea7b27
Stephen Kinnaird <slkinnaird@gmail.com>
2023-01-09 18:44:58 +0000

    dropdown menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
+import { useState } from 'react'
 import './NavBar.css'
 
 const NavBar = () => {
 
+  const [showList, setShowList] = useState(false)
+  const toggleButton = () => setShowList((current) => (!current));
+
+  function DropdownMenu(){
+    if(showList){
+    return(
+      <div className='dropdown'>
+        <a href='https://github.com/skinnaird88' target="_blank">GitHub</a>
+        <a href='../../SKinnaird-CV.pdf' target="_blank">CV</a>
+        <a href='https://www.linkedin.com/in/stephen-kinnaird-a9b713112/' target="_blank">LinkedIn</a>
+        <button onClick={toggleButton}>X</button>
+      </div>
+    )}
+    else{
+      return(
+        <a href="#" className='toggle-button' onClick={toggleButton}>
+        <span className='bar'></span>
+        <span className='bar'></span>
+        <span className='bar'></span>
+      </a>
+      )
+    }
+  }
+
+
   return (
     <nav>
+
+      <DropdownMenu/>
+    
       <a href='https://github.com/skinnaird88' target="_blank">
         <img src='../../github.png' id='github'></img>
       </a>
@@ -14,6 +43,8 @@ const NavBar = () => {
       <a href='https://www.linkedin.com/in/stephen-kinnaird-a9b713112/' target="_blank">
         <img src='../../linkedin.png' id='linkedin'></img>
       </a>
+
+        
 
     </nav>
   )
